80767413991321e17d96e822e5c43aa31a4aa593
Andrii <mecushka@gmail.com>
2023-03-16 23:30:57 +0200

    first one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        startBtn.disabled = true;
+    }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+    startBtn.disabled = false;
+    clearInterval(timerId);
+});
