e6c01dcbf9bb836f6148b2e7b339ab3ebd976736
Andrii <mecushka@gmail.com>
2023-03-17 00:06:51 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,18 +2,21 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-const startBtn = document.querySelector("[data-start]");
-const stopBtn = document.querySelector("[data-stop]");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 let timerId = null;
 
-startBtn.addEventListener("click", () => {
-    timerId = setInterval(() => {
-        document.body.style.backgroundColor = getRandomHexColor();
-        startBtn.disabled = true;
-    }, 1000);
-});
+startBtn.addEventListener('click', onStartGenerateColors);
+stopBtn.addEventListener('click', onStopGenerateColors);
 
-stopBtn.addEventListener("click", () => {
-    startBtn.disabled = false;
+function onStartGenerateColors() {
+    timerId = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function onStopGenerateColors() {
     clearInterval(timerId);
-});
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
